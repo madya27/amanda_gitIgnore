9be488ae70768cc8e42ab897f21ab4a7d92bfbde
mupied <mhmmadd.mufid@gmail.com>
2024-08-28 18:41:02 +0700

    Menambahkan Method Push

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const {getGoals, setGoals} = require('../controllers/goalControllers')
 
-router.get("/", (req, res) => {
-  res.status(200).json({
-    nama: "Amanda Madya Sabrina"
-  })
+router.get('/', getGoals)
+router.post('/', setGoals)
+
+router.put('/',(req,res) => {
+    res.json({
+        message: "Menambahkan method Put"
+    })    
 })
 
 module.exports = router;
